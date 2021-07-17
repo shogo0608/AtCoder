342f1cca3969ab461e8c1261dd085fbd9b3aa909
Shogo.I <shogo0608.i@gmail.com>
2021-07-18 00:11:45 +0900

    solve C of ABC210

--- ABC210/abc210_c.py
+++ ABC210/abc210_c.py
@@ -1,21 +1,23 @@
+from collections import defaultdict
+
 def main():
     N, K = [int(i) for i in input().split()]
     C = [int(i) for i in input().split()]
+    
+    answer = 0
+    now = 0
+    c_dict = defaultdict(int)
+    for i in range(N):
+        c_dict[C[i]] += 1
+        if c_dict[C[i]] == 1:
+            now += 1
+        if i >= K:
+            c_dict[C[i-K]] -= 1
+            if c_dict[C[i-K]] == 0:
+                now -= 1
+        answer = max(answer, now)
 
-    max = 0
-    flag = False
-    for i in range(N-K+1):
-        if flag and num < max:
-            flag = False
-            continue
-        flag = True
-        num = len(set(C[i:i+K]))
-        if num > max:
-            max = num
-        if max == K:
-            break
-
-    print(max)
+    print(answer)
 
 
 if __name__ == '__main__':
