052b0cc6b9a2dd440a6e66e855c2a56dded0c8d7
Shogo.I <shogo0608.i@gmail.com>
2021-07-24 16:41:54 +0900

    remove tqdm

--- ABC208/abc208_e.py
+++ ABC208/abc208_e.py
@@ -1,9 +1,7 @@
-from tqdm import tqdm
-
 N, K = map(int, input().split())
 
 answer = 0
-for n in tqdm(range(1, N+1)):
+for n in range(1, N+1):
     n_digits = list(map(int, str(n)))
     mul = 1
     for dig in n_digits:
