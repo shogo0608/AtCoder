82938073a9e9273c20381ae9cfcb14a143a77c58
Shogo.I <shogo0608.i@gmail.com>
2021-07-24 16:41:23 +0900

    TLE

---
+++ ABC208/abc208_e.py
@@ -0,0 +1,14 @@
+from tqdm import tqdm
+
+N, K = map(int, input().split())
+
+answer = 0
+for n in tqdm(range(1, N+1)):
+    n_digits = list(map(int, str(n)))
+    mul = 1
+    for dig in n_digits:
+        mul *= dig
+    if mul <= K:
+        answer += 1
+
+print(answer)
