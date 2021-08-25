33c2c182222b4bc50718bdab1807330d45c82941
Shogo.I <shogo0608.i@gmail.com>
2021-08-25 15:36:35 +0900

    AC

--- ABC206/abc206_c.py
+++ ABC206/abc206_c.py
@@ -1,3 +1,16 @@
-N = int(input().split())
+# 2021/08/25
+
+from collections import defaultdict
+
+N = int(input())
 A = list(map(int, input().split()))
 
+cnt_dict = defaultdict(int)
+for a in A:
+    cnt_dict[a] += 1
+ans = N * (N - 1) // 2
+for v in cnt_dict.values():
+    if v > 1:
+        ans -= v * (v - 1) // 2
+
+print(ans)
