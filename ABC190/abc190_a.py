c3b71cbbe685002eec11f1dc7a436618333b128a
Shogo.I <shogo0608.i@gmail.com>
2021-08-30 18:08:51 +0900

    AC

---
+++ ABC190/abc190_a.py
@@ -0,0 +1,13 @@
+# 2021/08/30
+
+A, B, C = map(int, input().split())
+
+if A > B:
+    print("Takahashi")
+elif A < B:
+    print("Aoki")
+else:
+    if C == 0:
+        print("Aoki")
+    else:
+        print("Takahashi")
