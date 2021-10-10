0b823f1eef26d2dd400fcf5e6b697c57c5309abd
Shogo.I <shogo0608.i@gmail.com>
2021-10-10 14:15:33 +0900

    Editorial TLE

--- ABC222/abc222_d.py
+++ ABC222/abc222_d.py
@@ -4,3 +4,17 @@ N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+upper_limit = max(B)
+dp = [[0] * (upper_limit + 1) for _ in range(N + 1)]
+dp[0] = [1] * (upper_limit + 1)
+for i in range(1, N + 1):
+    for j in range(upper_limit + 1):
+        if j >= A[i-1] and j <= B[i-1]:
+            if j == 0:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        else:
+            dp[i][j] = dp[i][j-1]
+
+print(dp[-1][-1] % MOD)
